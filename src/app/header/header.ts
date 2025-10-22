6e2c43b226cbc7d6a70fb7e3ef6c504774a50cbb
Alexandre Bastos <xande.dev@outlook.com>
2025-10-21 23:21:49 -0300

    :rocket: Iniciando primeiro projeto em Angular

---
+++ src/app/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  constructor() {}
+}
