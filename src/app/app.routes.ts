2783152f921ee8cd5a1b25da98e8b5570912dadf
Alexandre Bastos <xande.dev@outlook.com>
2025-10-23 22:36:42 -0300

    :construction: Atualizando título da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,17 @@ import { AboutPage } from './pages/about-page/about-page';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home', pathMatch: 'full'
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
   {
     path: 'home',
     component: HomePage,
+    title: 'Alexandre Bastos',
   },
   {
     path: 'about',
     component: AboutPage,
+    title: "Alexandre Bastos: sobre"
   },
 ];
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../../components/navbar/navbar';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'home-page',
