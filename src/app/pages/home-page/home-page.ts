bd05791f65be8942d02101fd18f04f5c65a7741b
Alexandre Bastos <xande.dev@outlook.com>
2025-10-25 14:59:26 -0300

    :construction: Feito layout inicial da home page do meu portfolio

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'home-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage {
+  constructor() {}
+
+  public socials = [
+    {
+      name: 'GitHub',
+      src: '/assets/icons/GitHub.png',
+      href: 'https://github.com/xand3',
+    },
+    {
+      name: 'linkedin',
+      src: '/assets/icons/linkedin.png',
+      href: 'https://www.linkedin.com/in/alexandr3-bastos/',
+    },
+    {
+      name: 'E-mail',
+      src: '/assets/icons/email.png',
+      href: 'mailto:xande.dev@outlook.com',
+    },
+    {
+      name: 'Beacons',
+      src: '/assets/icons/links.png',
+      href: 'https://beacons.ai/xande_dev',
+    },
+  ];
+}
